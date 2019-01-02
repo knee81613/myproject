28d3d12ade3483a1975f4dfd1fd6850ffac5020f
Reevan <abc81613@hotmail.com>
2019-01-02 23:56:43 +0800

    Initial commit

---
+++ src/main/java/com/pharmercy/Person.java
@@ -0,0 +1,8 @@
+package com.pharmercy;
+
+public class Person {
+public void hello() {
+    System.out.println("Hello world");
+
+}
+}
---
+++ src/main/kotlin/com/pharmercy/Hello.kt
@@ -0,0 +1,14 @@
+package com.pharmercy
+
+fun main(args: Array<String>) {
+//    println("Hello kotlin")
+//    Human().hello()
+    val h = Human()
+    h.hello()
+}
+
+class Human{
+fun hello(){
+println("Hello Kotlin")
+}
+}
